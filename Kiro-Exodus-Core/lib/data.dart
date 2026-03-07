0d4cab68d54c225d9a89ed9f7d4f793b07722294
YoskoBG <darkgt31252@outlook.com>
2026-03-07 20:42:21 +0300

    data.dart

---
+++ Kiro-Exodus-Core/lib/data.dart
@@ -0,0 +1,67 @@
+import './crypto.dart';
+
+List<Crypto> wallets = [
+  Crypto(
+    id: 'OMG',
+    name: 'OmiseGo',
+    currentPrice: 32.0,
+    holdings: 108.0,
+    active: true,
+  ),
+  Crypto(
+    id: 's',
+    name: 'OmiseGo',
+    currentPrice: 32.0,
+    holdings: 108.0,
+    active: true,
+  ),
+  Crypto(
+    id: 'OMG',
+    name: 'OmiseGo',
+    currentPrice: 32.0,
+    holdings: 108.0,
+    active: true,
+  ),
+  Crypto(
+    id: 's',
+    name: 'OmiseGo',
+    currentPrice: 32.0,
+    holdings: 108.0,
+    active: true,
+  ),
+  Crypto(
+    id: 'OMG',
+    name: 'OmiseGo',
+    currentPrice: 32.0,
+    holdings: 108.0,
+    active: true,
+  ),
+  Crypto(
+    id: 'BTC',
+    name: 'Bitcoin',
+    currentPrice: 0,
+    holdings: 0,
+    active: false,
+  ),
+  Crypto(
+    id: 'GNT',
+    name: 'Golem',
+    currentPrice: 0,
+    holdings: 0,
+    active: false,
+  ),
+  Crypto(
+    id: 'ETH',
+    name: 'Ethereum',
+    currentPrice: 0,
+    holdings: 0,
+    active: false,
+  ),
+  Crypto(
+    id: 'QTUM',
+    name: 'Quntum',
+    currentPrice: 0,
+    holdings: 0,
+    active: false,
+  ),
+];
