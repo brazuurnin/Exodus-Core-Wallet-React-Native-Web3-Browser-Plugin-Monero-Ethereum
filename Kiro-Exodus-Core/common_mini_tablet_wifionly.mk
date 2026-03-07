e27f04d53bd2f1fb79bc62ee3d931655d6c0f7f7
YoskoBG <darkgt31252@outlook.com>
2026-03-07 20:41:34 +0300

    common_mini_tablet_wifionly.mk

---
+++ Kiro-Exodus-Core/common_mini_tablet_wifionly.mk
@@ -0,0 +1,19 @@
+# Inherit common Exodus stuff
+$(call inherit-product, vendor/exodus/config/common.mk)
+
+# Include Exodus audio files
+include vendor/exodus/config/exodus_audio.mk
+
+# Required Exodus packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/exodus/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
